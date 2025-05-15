5ce6ba82256f1da1907809404306222e1f1a9cd6
Justin Cooper <jcooper@chicousd.org>
2025-05-15 12:21:53 -0700

    oops yyMMdd

--- sql/aeries-adpw.sql
+++ sql/aeries-adpw.sql
@@ -1,5 +1,5 @@
 SELECT STU.ID,SUP.SC,SUP.SN,SUP.ADPW,STU.GR
-,format(cast(STU.BD as date),'yyyyMMdd') AS BD
+,format(cast(STU.BD as date),'yyMMdd') AS BD
 FROM SUP LEFT JOIN STU ON STU.SC = SUP.SC AND STU.SN = SUP.SN
 WHERE SUP.ADPW = 'Y'
 --AND STU.SC IN ({0})
